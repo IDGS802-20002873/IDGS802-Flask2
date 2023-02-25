e7d6ad2ddd594ca25591d376c312f12fa6d7de4b
IDGS802-20002873 <20002873@alumnos.utleon.edu.mx>
2023-02-25 09:42:29 -0600

    Clase 25 de Febrero de 2023

--- app.py
+++ app.py
@@ -1,9 +1,50 @@
 from flask import Flask, render_template
 from flask import request
+from flask_wtf.csrf import CSRFProtect
+from flask import make_response
+from flask import flash
 
 import forms
 
 app=Flask(__name__)
+app.config['SECRET_KEY'] = 'Esta es una clave encriptada'
+csrf=CSRFProtect(app)
+
+@app.errorhandler(404)
+def no_encontrada(e):
+    return render_template('404.html')
+
+@app.before_request
+def before_request():
+    print('numero1')
+
+@app.route('/cookies', methods=['GET','POST'])
+def cookies():
+    print('numero2')
+    reg_user=forms.LoginForm(request.form)
+    datos = ''
+    if request.method=='POST' and reg_user.validate():
+        user=reg_user.username.data
+        password=reg_user.password.data
+        datos=user+'@'+password
+        success_message='Bienvenido {} '.format(user)
+        flash(success_message)  
+        response = make_response(render_template('cookies.html',form=reg_user))
+        response.set_cookie('username',user)
+        response.set_cookie('datos_user',datos)   
+        return response
+     
+    return render_template('cookies.html',form=reg_user)
+
+@app.after_request
+def after_request(response):
+    print('numero3')
+    return response
+
+@app.route('/saludo')
+def saludo():
+    nombre = request.cookies.get('username')
+    return render_template('saludo.html',nom=nombre)
 
 @app.route("/formulario2", methods=["GET"])
 def formulario2():
@@ -23,4 +64,5 @@ def Alumnos():
     return render_template("Alumnos.html", form=alum_form, mat=mat, nom=nom)
 
 if __name__ == "__main__":
+    csrf.init_app(app)
     app.run(debug=True, port=3000)
--- forms.py
+++ forms.py
@@ -4,6 +4,10 @@ from wtforms import StringField, SubmitField, FieldList, FormField, SelectField,
 from wtforms.fields import EmailField, PasswordField
 from wtforms import validators
 
+def mi_validacion(form, field):
+    if len(field.data)==0:
+        raise validators.ValidationError['El campo no tiene datos']
+
 class UserForm(Form):
     
     matricula = StringField('Matricula',[
@@ -11,8 +15,13 @@ class UserForm(Form):
     nombre = StringField('Nombre',[
         validators.DataRequired(message= 'El nombre es requerido'),
         validators.length(min=15, message='Ingresa un valor maximo')])
-    apaterno = StringField('Apaterno')
-    amaterno = StringField('Amaterno')
+    apaterno = StringField('Apaterno',[mi_validacion])
+    amaterno = StringField('Amaterno',[
+        validators.DataRequired(message= 'El apellido Materno es requerida')])
     email = EmailField('Correo',[
-        validators.DataRequired(message= 'Ingresa un valor maximo')])
+        validators.DataRequired(message= 'El correo es requerido')])
     
+
+class LoginForm(Form):
+    username = StringField('Usuario',[validators.DataRequired(message= 'El usuario es requerida')])
+    password = PasswordField('Contraseña',[validators.DataRequired(message= 'El campo es requerido'),validators.length(min=15, message='Ingresa un valor maximo')])
